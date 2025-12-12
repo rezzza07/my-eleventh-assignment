f348d4104c8f311f5f8cf6b4c72b94076f348658
rezzza07 <notfunnyatall0007@gmail.com>
2025-12-13 01:59:29 +0600

    All Books

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import { RouterProvider } from "react-router/dom";
 import './index.css'
 import App from './App.jsx'
 import { router } from './routes/Router.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    
-
+    </QueryClientProvider>
   </StrictMode>,
 )
--- src/pages/All Books/AllBooks.jsx
+++ src/pages/All Books/AllBooks.jsx
@@ -1,9 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { Link } from 'react-router';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
 
 const AllBooks = () => {
+
+    const axiosSecure = useAxiosSecure();
+
+    const { data: books = [] } = useQuery({
+        queryKey: ['all-books'],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/books`);
+            return res.data;
+        }
+    })
+
     return (
-        <div>
-            wow
+
+        <div className="p-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {books.map(book => (
+                <Link to={`/books/${book._id}`} key={book._id}>
+                    <div className="card bg-primary text-primary-content shadow-xl hover:scale-105 duration-300">
+                        <figure>
+                            <img src={book.image} alt={book.name} className="h-60 w-full object-cover" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{book.name}</h2>
+                            <p className="text-secondary">Author: {book.author}</p>
+                            <p>Status: <span className="badge badge-secondary">{book.status}</span></p>
+                            <p className="font-bold">Price: ${book.price}</p>
+                        </div>
+                    </div>
+                </Link>
+            ))}
         </div>
     );
 };
